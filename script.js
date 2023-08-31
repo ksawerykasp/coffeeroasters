c1ce36f900d7a80f68bf0cc48445064d888157b8
Ksawery Kasprzycki <ksawery.kasprzyckix@gmail.com>
2023-08-31 16:41:19 +0200

    Add hidden overflow to the body

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const toggleButton = document.querySelector(".navbar__hamburger-button");
 
-const toggleActive = () =>
-{
-    const toggleMenu = document.querySelector(".navbar__toggleMenu")
-    const toggleButtonIcon = document.querySelector(".navbar__button-line")
-    toggleMenu.classList.toggle("navbar__toggleMenu--active")
-    toggleButtonIcon.classList.toggle("navbar__button-line--active")
-}
+const toggleActive = () => {
+  const toggleMenu = document.querySelector(".navbar__toggleMenu");
+  const toggleButtonIcon = document.querySelector(".navbar__button-line");
+  toggleMenu.classList.toggle("navbar__toggleMenu--active");
+  toggleButtonIcon.classList.toggle("navbar__button-line--active");
+  document.body.classList.toggle("overflow-body");
+};
 
 toggleButton.addEventListener("click", toggleActive);
